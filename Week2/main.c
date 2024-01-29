8b21b1fcc975e423815c64a8d3c9b1f65bf56a7a
cjoshua <cjoshua@chalmers.se>
2024-01-29 22:47:38 +0100

    bug fix

--- Week2/main.c
+++ Week2/main.c
@@ -1,14 +1,15 @@
-#include <stdio.h> //Used for printf()
-#include <unistd.h> //Used for sleep()
-
+#include <stdio.h>  //Used for printf()
+#include <stdlib.h> //Used for atoi()
+// #include <unistd.h> //Used for sleep()
+#include <windows.h> //Used for Sleep()
 
 enum days_of_the_week
 {
-    MONDAY, // by declaring the first element as one it will start counting from one instead of zero
-    TUESDAY,    // hold value 2
-    WEDNESDAY,  // hold value 3
-    THURSDAY,   // hold value 4
-    FRIDAY,     // hold value 5
+    MONDAY = 1,    // by declaring the first element as one it will start counting from one instead of zero
+    TUESDAY,   // hold value 2
+    WEDNESDAY, // hold value 3
+    THURSDAY,  // hold value 4
+    FRIDAY,    // hold value 5
     SATURDAY,
     SUNDAY,
     NUMBER_OF_DAYS
@@ -16,14 +17,13 @@ enum days_of_the_week
 
 enum weeks
 {
-    WEEK1,
+    WEEK1 = 1,
     WEEK2,
     WEEK3,
     WEEK4,
     WEEK5,
     NUMBER_OF_WEEKS
 };
-
 
 enum weeks getStartingWeek()
 {
@@ -45,27 +45,27 @@ void printWeekAndDay(enum weeks week, enum days_of_the_week today)
     printf("Week: %d, ", week);
     switch (today)
     {
-        case 1:
-            printf("Monday\n");
-            break;
-        case 2:
-            printf("Tuesday\n");
-            break;
-        case 3:
-            printf("Wednesday\n");
-            break;
-        case 4:
-            printf("Thursday\n");
-            break;
-        case 5:
-            printf("Friday\n");
-            break;
-        case 6:
-            printf("Saturday\n");
-            break;
-        case 7:
-            printf("Sunday\n");
-            break;
+    case 1:
+        printf("Monday\n");
+        break;
+    case 2:
+        printf("Tuesday\n");
+        break;
+    case 3:
+        printf("Wednesday\n");
+        break;
+    case 4:
+        printf("Thursday\n");
+        break;
+    case 5:
+        printf("Friday\n");
+        break;
+    case 6:
+        printf("Saturday\n");
+        break;
+    case 7:
+        printf("Sunday\n");
+        break;
     }
 }
 
@@ -77,16 +77,14 @@ int main()
     printf("Provide Starting day: ");
     enum days_of_the_week startingDay = getStartingDay();
 
-
-    for (int i = startingWeek; i <= NUMBER_OF_WEEKS; i++)
+    for (int i = startingWeek; i < NUMBER_OF_WEEKS; i++)
     {
-        for (int j = startingDay; j <= NUMBER_OF_DAYS; j++)
+        for (int j = startingDay; j < NUMBER_OF_DAYS; j++)
         {
             printWeekAndDay(i, j);
-            sleep(1);
+            Sleep(1000);
         }
+        startingDay = MONDAY;
+
     }
 }
-
-
-
