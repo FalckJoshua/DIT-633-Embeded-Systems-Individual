181079240b9c3d03720a51fdb6b99d02f0743bec
cjoshua <cjoshua@chalmers.se>
2024-02-08 00:11:56 +0100

    bonus 3

---
+++ week3/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>  //Used for printf()
+#include <stdlib.h> //Used for malloc()
+
+// GLOBAL VARIABLES
+int matrix_x; // declare global variable to store the number of rows
+int matrix_y; // declare global variable to store the number of columns
+
+// function to prints the matrix
+void printMatrix(int **matrix)
+{
+
+    for (int i = 0; i < matrix_x; i++) // loop through the rows
+    {
+        for (int j = 0; j < matrix_y; j++) // loop through the columns
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// function to add two matrices
+void addMatrix(int **matrix1, int **matrix2)
+{
+    int **result = (int **)malloc(matrix_x * sizeof(int *)); // allocate memory for the result matrix
+    for (int i = 0; i < matrix_x; i++)                       // loop through the rows
+    {
+        result[i] = (int *)malloc(matrix_y * sizeof(int)); // allocate memory for the columns of the result matrix
+    }
+
+    for (int i = 0; i < matrix_x; i++) // loop through the rows
+    {
+        for (int j = 0; j < matrix_y; j++) // loop through the columns
+        {
+            result[i][j] = matrix1[i][j] + matrix2[i][j]; // add the elements of the two matrices and store the result in the result matrix
+        }
+    }
+    printf("The sum is:\n");
+    printMatrix(result);
+}
+
+// function to create a matrix
+int **matrixMaker()
+{
+    int **matrix = (int **)malloc(matrix_x * sizeof(int *)); // allocate memory for the matrix
+    for (int i = 0; i < matrix_x; i++)                       // loop through the rows
+    {
+        matrix[i] = (int *)malloc(matrix_y * sizeof(int)); // allocate memory for the columns of the matrix
+    }
+
+    for (int i = 0; i < matrix_x; i++) // loop through the rows
+    {
+        for (int j = 0; j < matrix_y; j++) // loop through the columns
+        {
+            scanf("%d", &matrix[i][j]); // store the input in the matrix
+        }
+    }
+    return matrix; // return the matrix
+}
+
+// function to scan the size of the matrix
+void scanMatrixSize()
+{
+    scanf("%d", &matrix_x); // store the input in the global variable matrix_x
+    scanf("%d", &matrix_y); // store the input in the global variable matrix_y
+}
+
+// function to check the size of the matrix
+void checkMatrixSize()
+{
+    if ((matrix_x < 2 || matrix_x > 4) || (matrix_y < 2 || matrix_y > 4)) // check if the size of the matrix is valid
+    {
+        printf("invalid");
+        exit(0); // exit the program
+    }
+}
+
+// main function
+int main()
+{
+    scanMatrixSize();              // call the function to scan the size of the matrix
+    checkMatrixSize();             // call the function to check the size of the matrix
+    int **matrix1 = matrixMaker(); // call the function to create the first matrix
+    int **matrix2 = matrixMaker(); // call the function to create the second matrix
+    addMatrix(matrix1, matrix2);   // call the function to add the two matrices
+    return 0;                      // return 0 to exit the program
+}
